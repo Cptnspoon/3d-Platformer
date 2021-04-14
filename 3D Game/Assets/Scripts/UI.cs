f2a770199daf0dc8fd04d26770beccc526ae13a2
Cptnspoon <cptndbag@gmail.com>
2021-04-13 19:40:30 -0500

    testing branch

--- 3D Game/Assets/Scripts/UI.cs
+++ 3D Game/Assets/Scripts/UI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+//Just a test
 public class UI : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
