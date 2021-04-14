97426b17187ae8e989f8c150f6c1690ed133f88f
Cptnspoon <cptndbag@gmail.com>
2021-04-13 19:48:12 -0500

    Update UI.cs

--- 3D Game/Assets/Scripts/UI.cs
+++ 3D Game/Assets/Scripts/UI.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+<<<<<<< Updated upstream
 
+=======
+//Just a test
+//another test
+
+//final test
+>>>>>>> Stashed changes
 public class UI : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
